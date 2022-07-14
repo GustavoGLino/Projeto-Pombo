9231a8f51934648d8c0006ee518a43013f0eabb0
guest01 <guest01@FLE207D21.alunos.sc.senac.br>
2022-07-13 21:46:40 -0300

    Novo Projeto Poo

---
+++ projetoPombo/src/entidade/Usuario.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package entidade;
+
+/**
+ * @author guest01
+ *
+ */
+public class Usuario {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
